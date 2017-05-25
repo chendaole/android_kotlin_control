519763c5353c992e3e75b970a010f3c4a530ed68
chendaole <chendaole@4399inc.com>
2017-05-25 18:20:21 +0800

    add:page

--- app/src/main/java/com/exp/chendaole/myapplication/MainActivity.kt
+++ app/src/main/java/com/exp/chendaole/myapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.exp.chendaole.myapplication
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
@@ -7,6 +8,7 @@ import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,6 +23,14 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+
+        val iceboxButton = findViewById(R.id.icebox) as Button
+        iceboxButton.setOnClickListener{ view ->
+            val intent: Intent = Intent()
+            intent.setClass(this, devices_Activity::class.java)
+            startActivity(intent)
+        }
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
